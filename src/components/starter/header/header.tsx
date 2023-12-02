14a06c69be5186c541406ff6c5b59bb4a93f7cf8
Riajul Islam <kmriajulislami@gmail.com>
2023-12-02 17:08:26 +0600

    feat: use <Link>

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,15 +1,16 @@
 import { component$ } from "@builder.io/qwik";
 import { QwikLogo } from "../icons/qwik";
 import styles from "./header.module.css";
+import {Link} from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <header class={styles.header}>
       <div class={["container", styles.wrapper]}>
         <div class={styles.logo}>
-          <a href="/" title="qwik">
+          <Link href="/" title="qwik">
             <QwikLogo height={50} width={143} />
-          </a>
+          </Link>
         </div>
         <ul>
           <li>
